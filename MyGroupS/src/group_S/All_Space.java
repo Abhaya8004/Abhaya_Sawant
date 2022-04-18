ee8546ef8f7833854371f7de3e31efd87e2ce57c
Abhaya8004 <sawantabhaya89@gmail.com>
2022-04-18 17:23:24 +0530

    My first commit
    This is my first attempt

---
+++ MyGroupS/src/group_S/All_Space.java
@@ -0,0 +1,12 @@
+package group_S;
+
+public class All_Space {
+	
+	public static void main(String[]args)
+	{
+		System.out.println("Hello Friends");
+		System.out.println("GoodEvng");
+		System.out.println("welcome to my place");
+	}
+
+}
